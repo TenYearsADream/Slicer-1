71447e02542533943aa0e9626a3ec038196a4325
xywc <xjtu_xi_19_504@example.com>
2018-07-13 12:36:01 +0800

    version2

---
+++ readstlfile.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include<vector>
+#include"point3f.h"
+using namespace std;
+
+class ReadSTLFile
+{
+public:
+    bool ReadStlFile(const char *cfilename);
+    int NumTri();
+    vector<Point3f>& PointList();
+private:
+    vector<Point3f> pointList;
+    unsigned int unTriangles;
+    bool ReadASCII(const char *cfilename);
+    bool ReadBinary(const char *cfilename);
+
+    char* memwriter;
+    int cpyint(const char*& p);
+    float cpyfloat(const char*& p);
+};
